434c74bd2a9d0f66ad9744c3fec6992f3c92d281
Julie <julie.kodehode@gmail.com>
2026-09-05 13:16:32 +0200

    feat: implement debug toggle button and styling for debug zone

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,41 @@
 import styles from "./app.module.css";
+import { useState } from "react";
 
 function App() {
+  const [showDebug, setShowDebug] = useState(false);
 
   return (
     <>
       <h1>Stardew Valley Tracker</h1>
       
       {/* DEBUG ZONE */}
-      <section className={styles.debugZone}>
-        {/* DEBUG VILLAGER CARDS */}
-        <div className={styles.debugCard}>
-          <img className={styles.villagerPortrait} src="" alt="" />
-          <h3>Pam</h3>
-          <ul className={styles.giftGrid}>
-            <li className={styles.giftList}>Beer, Cactus Fruit, Glazed Yams, Mead, Pale Ale, Parsnip, Tropical Curry</li>
-          </ul>
-        </div>
+      <button
+					onClick={() => setShowDebug(!showDebug)}
+					// Combining classNames to dynamically change color.
+					className={`${styles.debugButton} ${showDebug ? styles.buttonOn : styles.buttonOff}`}>
+					{showDebug ? "Hide Debug" : "Show Debug"}
+				</button>
 
-        <div className={styles.debugCard}>
-          <img className={styles.villagerPortrait} src="" alt="" />
-          <h3>Linus</h3>
-          <ul className={styles.giftGrid}>
-            <li className={styles.giftList}>Blueberry Tart, Cactus fruit, Coconut, Dish o' The Sea, Yam</li>
-          </ul>
-        </div>
-      </section>
+      {showDebug && (
+        <section className={styles.debugZone}>
+          {/* DEBUG VILLAGER CARDS */}
+          <div className={styles.debugCard}>
+            <img className={styles.villagerPortrait} src="" alt="" />
+            <h3>Pam</h3>
+            <ul className={styles.giftGrid}>
+              <li className={styles.giftList}>Beer, Cactus Fruit, Glazed Yams, Mead, Pale Ale, Parsnip, Tropical Curry</li>
+            </ul>
+          </div>
+
+          <div className={styles.debugCard}>
+            <img className={styles.villagerPortrait} src="" alt="" />
+            <h3>Linus</h3>
+            <ul className={styles.giftGrid}>
+              <li className={styles.giftList}>Blueberry Tart, Cactus fruit, Coconut, Dish o' The Sea, Yam</li>
+            </ul>
+          </div>
+        </section>
+      )}
     </>
   );
 };
